3c91567a15b4ca75c01bec6043b924b46d225315
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-09-26 20:24:40 -0700

    Now only one dropdown can be opened at a time

--- drop-down-menu/script.js
+++ drop-down-menu/script.js
@@ -1,9 +1,25 @@
 const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
-dropdownToggles.forEach((element) => {
-  element.addEventListener('click', () => {
-    element.nextElementSibling.style.display === 'flex'
-      ? (element.nextElementSibling.style.display = 'none')
-      : (element.nextElementSibling.style.display = 'flex');
+dropdownToggles.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (btn.nextElementSibling.classList.contains('show')) {
+      closeOpenItems();
+    } else {
+      closeOpenItems();
+      btn.nextElementSibling.classList.toggle('show');
+    }
   });
+});
+
+function closeOpenItems() {
+  const openMenus = document.querySelectorAll('.dropdown-menu');
+  openMenus.forEach(function (menus) {
+    menus.classList.remove('show');
+  });
+}
+
+document.addEventListener('click', (e) => {
+  if (!e.target.matches('.dropdown-toggle')) {
+    closeOpenItems();
+  }
 });
